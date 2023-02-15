2b94aaef2643e9f71f9b951f959b0048c8878339
Lukas Tietenberg <sourcefranke@users.noreply.github.com>
2023-02-15 14:28:38 +0100

    Rearrangement

--- util/plotter.py
+++ util/plotter.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 
-def plot(data, random_data, title):
+def plot_data_random(data, random_data, title):
     """
     :param data: dictionary to be plotted
     :param random_data:
@@ -12,5 +12,20 @@ def plot(data, random_data, title):
     plt.ylabel("Occurrence")
     plt.plot(data.keys(), data.values(), color="r", label='formula')
     plt.stem(random_data.keys(), random_data.values(), label='random')
+    plt.legend()
+    plt.show()
+
+
+def plot_data_adv(data_normal, data_adv, title):
+    """
+    :param data_normal: dictionary to be plotted
+    :param data_adv: same count of dice, just with advantage
+    :param title: individual title for the diagram
+    """
+    plt.title(title)
+    plt.xlabel("Roll")
+    plt.ylabel("Occurrence")
+    plt.plot(data_normal.keys(), data_normal.values(), color="b", label='normal')
+    plt.plot(data_adv.keys(), data_adv.values(), color="r", label='advantage')
     plt.legend()
     plt.show()
